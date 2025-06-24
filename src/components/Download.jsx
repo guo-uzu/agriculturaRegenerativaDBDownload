7bdf00ae7b84d6ebee5f104fd052bcc1f4f5fa18
Alejandro Camacho Olivares <alejandro@uzu.digital>
2025-06-24 14:34:48 -0600

    Changes done

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import { initializeApp } from 'firebase/app'
-import { collection, query, getDocs, getFirestore } from 'firebase/firestore';
-import { mkConfig, generateCsv, download } from 'export-to-csv';
+import React from "react";
+import { initializeApp } from "firebase/app";
+import { collection, query, getDocs, getFirestore } from "firebase/firestore";
+import { mkConfig, generateCsv, download } from "export-to-csv";
 
 export default function Download() {
   const firebaseConfig = {
-    apiKey: import.meta.env.PUBLIC_API_KEY ,
+    apiKey: import.meta.env.PUBLIC_API_KEY,
     authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
     projectId: import.meta.env.PUBLIC_PROJECT_ID,
     storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.PUBLIC_MESSAGING_SENDER_ID,
     appId: import.meta.env.PUBLIC_API_ID,
-    measurementId: import.meta.env.PUBLIC_MEASUREMENT_ID
+    measurementId: import.meta.env.PUBLIC_MEASUREMENT_ID,
   };
-  const deleteComma = string => string.replace(",", "")
+
+  const deleteComma = (string) =>
+    string !== undefined ? string.replace(",", "") : "";
   const csvConfig = mkConfig({
-    useKeysAsHeaders: true
-  })
-  let dataComplete = []
+    useKeysAsHeaders: true,
+  });
+  let dataComplete = [];
 
   const startProcess = async () => {
-    const app = initializeApp(firebaseConfig)
-    const db = getFirestore(app)
-    const q = query(collection(db, "usersGestionDeProyectos"))
-    const querySnapshot = await getDocs(q)
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const q = query(collection(db, "usersSaludSuelos"));
+    const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       const {
         cargo,
@@ -32,34 +34,33 @@ export default function Download() {
         medioWebinar,
         name,
         organization,
-        partWebinar,
+        partWebinarOpt,
         participationWebinar,
         proxWebinar,
         sector,
-
-      } = doc.data()
+      } = doc.data();
 
       const dataInd = {
         Nombre: name,
         Apellido: lastName,
         Email: email,
         Organizacion: deleteComma(organization),
-        ParticipacionUltWebinarDato: partWebinar,
+        ParticipacionUltWebinarDato: partWebinarOpt,
         MedioWebinar: deleteComma(medioWebinar),
         ParticipationUltWebinarSiNo: participationWebinar,
         Proximo: proxWebinar,
         Cargo: deleteComma(cargo),
-        Sector: deleteComma(sector)
-      }
-      dataComplete.push(dataInd)
-    })
-    const csv = generateCsv(csvConfig)(dataComplete)
-    download(csvConfig)(csv)
-  }
+        Sector: deleteComma(sector),
+      };
+      dataComplete.push(dataInd);
+    });
+    const csv = generateCsv(csvConfig)(dataComplete);
+    download(csvConfig)(csv);
+  };
 
   return (
     <>
       <button onClick={startProcess}>Descarga la DB</button>
     </>
-  )
+  );
 }
